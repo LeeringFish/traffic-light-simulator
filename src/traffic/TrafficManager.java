bca32303acbe45d11fd22b57a78b082dff970a85
LeeringFish <theleeringfish@proton.me>
2025-12-21 20:31:51 -0500

    add TrafficManager.java

---
+++ src/traffic/TrafficManager.java
@@ -0,0 +1,13 @@
+package traffic;
+
+public class TrafficManager {
+
+    public static void printMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Add road");
+        System.out.println("2. Delete road");
+        System.out.println("3. Open system");
+        System.out.println("0. Quit");
+    }
+
+}
